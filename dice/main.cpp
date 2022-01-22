078f0a4d23540de61760d6c4612b16184162b63a
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-22 06:41:19 +0300

    Initial Commit

---
+++ dice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dice
+//
+//  Created by Jedidiah Uchenna on 22.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
